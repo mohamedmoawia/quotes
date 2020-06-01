8a9024e127bb5848f44b9858e62ef73fb5951536
mohamedmoawia <mo3wia94@gmail.com>
2020-06-01 13:22:14 +0200

    Updates

---
+++ precache-manifest.cb560bf296bcfd97787aa0d580db46ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcff366cc7325d788a31f5b2bb254599",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "7afc49cff0e59a905e83",
+    "url": "/quotes/static/css/main.eb7f774b.chunk.css"
+  },
+  {
+    "revision": "cf82120b44ed372591cf",
+    "url": "/quotes/static/js/2.1c5717ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/static/js/2.1c5717ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afc49cff0e59a905e83",
+    "url": "/quotes/static/js/main.274d716d.chunk.js"
+  },
+  {
+    "revision": "89ff4ab3827633602522",
+    "url": "/quotes/static/js/runtime-main.a099f914.js"
+  }
+]);
